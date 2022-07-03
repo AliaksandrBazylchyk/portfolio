e7af1cab3694182f0390bc39b8df530552aada9f
Aliaksandr Bazylchyk <bazylchyk.a@gmail.com>
2022-07-03 18:06:02 +0300

    Configured ROUTEs

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.scss'
 import App from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />}>
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 )
