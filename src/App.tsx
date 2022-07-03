5b9248c00e6064144f6d0c9dc6bf95d7440c958e
Aliaksandr Bazylchyk <bazylchyk.a@gmail.com>
2022-07-03 18:34:50 +0300

    Created generic header [unfinished]

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+import Header from "./components/Header";
+
 import './App.scss'
 
-const App = () => {
-    return <div className="App" />
+const App: React.FC = () => {
+    return (
+        <div>
+            <Header />
+            <Outlet />
+        </div>
+    )
 }
 
 export default App
