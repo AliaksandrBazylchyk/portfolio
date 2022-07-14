2427f85c7134e35fe0e2ac81eafdded215f59a69
Aliaksandr Bazylchyk <bazylchyk.a@gmail.com>
2022-07-14 12:30:20 +0300

    Deleted header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
-import Header from "@components/Header";
+// import Header from "@components/Header";
 
 import './App.scss'
 
 const App: React.FC = () => {
     return (
         <div>
-            <Header />
+            {/*<Header />*/}
             <Outlet />
         </div>
     )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 import './styles.scss'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 const Header: React.FC = () => {
     return (
         <div className="header">
             <div className="header__text-handler">
-                <Link to={'/'}>
-                    <span className="header__text-handler__text">Home</span>
-                </Link>
+                {/*<Link to={'/'}>*/}
+                {/*    <span className="header__text-handler__text">Home</span>*/}
+                {/*</Link>*/}
             </div>
             <div className="header__button-handler">
                 {/*<button>skills-page</button>*/}
